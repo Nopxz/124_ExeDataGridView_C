3db33d5a4826c61af89c6d4e1f3c746e742ee66e
Nopxz <naufalps42@gmail.com>
2022-04-08 13:39:57 +0700

    menambahkan background pada isi tabel

--- Exercise2PABD/Form1.cs
+++ Exercise2PABD/Form1.cs
@@ -25,6 +25,11 @@ namespace Exercise2PABD
             //membuat tulisan header tabel bold (tebal)
             dataGridView1.ColumnHeadersDefaultCellStyle.Font = new Font(dataGridView1.ColumnHeadersDefaultCellStyle.Font, FontStyle.Bold);
 
+            this.dataGridView1.Columns[0].DefaultCellStyle.SelectionBackColor = Color.Pink;
+            this.dataGridView1.Columns[1].DefaultCellStyle.SelectionBackColor = Color.Orange;
+            this.dataGridView1.Columns[2].DefaultCellStyle.SelectionBackColor = Color.Green;
+            this.dataGridView1.Columns[3].DefaultCellStyle.SelectionBackColor = Color.Blue;
+            this.dataGridView1.Columns[4].DefaultCellStyle.SelectionBackColor = Color.Maroon;
         }
     }
 }
